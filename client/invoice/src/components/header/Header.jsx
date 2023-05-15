bd0a5fc08aac1ae0074dbf66caa1ca6259ee5fb9
zac <antonykariukikarume@gmail.com>
2023-05-15 22:23:38 +0300

    Header frontend

--- client/invoice/src/components/header/Header.jsx
+++ client/invoice/src/components/header/Header.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 import "./header.css"
+import { Link } from "react-router-dom";
+import logo from "../../imgs/ignite_nursing_logo.png"
+import { BiPlus, BiChevronDown } from 'react-icons/bi';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <section className="header-menu">
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-12">
+            <nav className="main-header">
+              <div className="left">
+                <Link to={"/"} style={{ textDecoration: "none", color: "inherit" }}>
+                  <img src={logo} alt="Ignite Nursing" />
+                </Link>
+              </div>
+              <div className="right">
+                <ul className="menu">
+                  <li>
+                    <BiPlus className="plus" />
+                  </li>
+                  <li>
+                    Account <BiChevronDown className="arrow" />
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
