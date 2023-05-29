95191bdf4f69c5934b6383fd4e8b88c840e6488b
zac <antonykariukikarume@gmail.com>
2023-05-29 10:47:08 +0300

    Creating the upload models

---
+++ server/routes/upload.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const BusinessUnit = require("../models/BusinessUnit");
+const Candidate = require("../models/Candidate");
+const ClientInvoice = require("../models/ClientInvoice");
+const Payroll = require("../models/Payroll");
